67006f9d80cb579a59ae265eb17c34934c7d69cf
dan kuebrich <dan.kuebrich@gmail.com>
2011-12-17 00:48:37 +0000

    [register] working registration script

--- projects/reddit/test_scripts/register.py
+++ projects/reddit/test_scripts/register.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 import mechanize
 import cookielib
 import urllib
@@ -12,12 +11,17 @@ from reddit_settings import *
 class Transaction(object):
     def __init__(self):
         self.custom_timers = {}
-    
-    def run(self):
-        br = init_browser()
 
-        login(br, USER, PASS)
-        # TODO
+    def run(self):
+        (cj, br) = init_browser()
+
+        br.open(BASE_URL)
+
+        br.select_form(predicate=lambda f: 'id' in f.attrs and f.attrs['id'] == 'login_reg')
+        br.form['user'] = 'mech2'
+        br.form['passwd'] = 'reddit'
+        br.form['passwd2'] = 'reddit'
+        br.submit()
 
 if __name__ == '__main__':
     trans = Transaction()
