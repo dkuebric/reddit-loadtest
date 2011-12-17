64e9707343e48f0c25efeb56cb51f0f35d4ecde7
dan kuebrich <dan.kuebrich@gmail.com>
2011-12-17 00:55:25 +0000

    [register] allow for batch-creation of users with predictable numeric names

--- projects/reddit/test_scripts/register.py
+++ projects/reddit/test_scripts/register.py
@@ -12,18 +12,26 @@ class Transaction(object):
     def __init__(self):
         self.custom_timers = {}
 
-    def run(self):
-        (cj, br) = init_browser()
+    def run(self, basename='user_', num=1):
+        for i in xrange(num):
+            uname = basename + str(i+1)
 
-        br.open(BASE_URL)
+            (cj, br) = init_browser()
 
-        br.select_form(predicate=lambda f: 'id' in f.attrs and f.attrs['id'] == 'login_reg')
-        br.form['user'] = 'mech2'
-        br.form['passwd'] = 'reddit'
-        br.form['passwd2'] = 'reddit'
-        br.submit()
+            br.open(BASE_URL)
+            br.select_form(predicate=lambda f: 'id' in f.attrs and f.attrs['id'] == 'login_reg')
+            br.form['user'] = uname
+            br.form['passwd'] = 'reddit'
+            br.form['passwd2'] = 'reddit'
+            br.submit()
+            print "Created user " + uname
 
 if __name__ == '__main__':
     trans = Transaction()
-    trans.run()
+    if len(sys.argv) == 3:
+        basename = sys.argv[1]
+        num = int(sys.argv[2])
+        trans.run(basename, num)
+    else:
+        trans.run()
     print trans.custom_timers
